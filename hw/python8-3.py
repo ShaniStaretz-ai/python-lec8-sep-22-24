f48b56cc60d5c2721ff551842007810c2d840633
Shani Staretz <shakimi88@gmail.com>
2024-09-23 15:59:08 +0300

    align

--- hw/python8-3.py
+++ hw/python8-3.py
@@ -1,20 +1,20 @@
 l3: list[int] = []
 MULTI_10 = 10
 SENTINEL: int = -999
-for i in range(MULTI_10, MULTI_10 ** 2 +1, MULTI_10):
+for i in range(MULTI_10, MULTI_10 ** 2 + 1, MULTI_10):
     l3.append(i)
 print(l3)
 
 while True:
     number: int = int(input("Enter number:"))
-    if number == SENTINEL: #break point
+    if number == SENTINEL:  # break point
         break;
-    
-    if number<=10:
-        index=0
+
+    if number <= 10:
+        index = 0
     else:
         index = number // MULTI_10 - 1  # since the values are in multiples 10,
     while l3[index] < number:  # find the right index
         index += 1
-    l3.insert(index, number) #when find the right index add to the list
+    l3.insert(index, number)  # when find the right index add to the list
     print(l3)
